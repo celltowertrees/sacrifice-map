b32cb6ebe0675386c47741bdd19adc208f96ef40
dataface <monson.exe@gmail.com>
2015-09-18 16:59:39 -0400

    the simplest server ever known to humankind

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-var d3 = require('d3'),
-    animals = require('./animals.json'),
-    geo = require('./nycboroughboundaries.geojson'),
-    topo = require('topojson');
-
+var http = require('http'),
     
+    // packages
+    d3 = require('d3'),
+    topo = require('topojson'),
+    
+    // data
+    animals = require('./animals.json'),
+    geo = require('./nycboroughboundaries.geojson');
+
+// Does It Work?
 var animalTypes = animals.map(function (animal) {
         return animal.animal;
     }),
     topology = topo.topology({collection: geo});
 
-console.log(topology);
+// server stuff
+
+var PORT = 8080;
+
+var handleRequest = function (request, response) {
+        response.end('<h1>Animals</h1>' + animalTypes);
+    },
+    server = http.createServer(handleRequest);
+
+server.listen(PORT, function () {
+    console.log('Server listening on: http://localhost:%s', PORT);
+});
