58440036166f43a474970af0f9d8cea2bc036301
dataface <monson.exe@gmail.com>
2015-09-17 22:23:53 -0400

    add NYC boundary geoJSON

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var d3 = require('d3'),
     animals = require('./animals.json'),
+    geo = require('./nycboroughboundaries.geojson'),
     topo = require('topojson');
 
     
 var animalTypes = animals.map(function (animal) {
         return animal.animal;
-    });
+    }),
+    topology = topo.topology({collection: geo});
 
-console.log(animalTypes);
+console.log(topology);
