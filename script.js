fc11b80f4430807d606777942e477af3fc5e1b6b
dataface <monson.exe@gmail.com>
2015-09-18 20:52:17 -0400

    ensure that our artificial DOM pre-renders correctly

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 var http = require('http'),
+    jsdom = require('jsdom'),
     
     // packages
     d3 = require('d3'),
-    topo = require('topojson'),
+    topojson = require('topojson'),
     
     // data
     animals = require('./animals.json'),
-    geo = require('./nycboroughboundaries.geojson');
+    nyc = require('./nycboroughboundaries.geojson');
 
-// Does It Work?
-var animalTypes = animals.map(function (animal) {
-        return animal.animal;
-    }),
-    topology = topo.topology({collection: geo});
+// =================================================== convert stuff
 
-// server stuff
+var topology = topojson.topology({collection: nyc[0]});
+
+// =================================================== create artificial DOM
+
+var htmlStub = '<html><head><title>lol</title></head><body><div id="container"></div><script src="node_modules/d3/d3.min.js"></script></body></html>',
+    result;
+
+jsdom.env({
+    features: {
+        QuerySelector: true
+    },
+    html: htmlStub,
+    done: function (errors, window) {
+        // im sure i decided to run this from the server for good reason.
+        // d3 wants the DOM to be available. so we are simulating a client-side env.
+        zone = window.document.querySelector('#container');
+        d3.select(zone).append('svg')
+            .attr('width', 960)
+            .attr('height', 1160);
+        
+
+        // convert to string
+        result = window.document.querySelector('body').innerHTML;
+    }
+});
+
+// =================================================== draw
+
+//svg.append('path')
+//    .datum(topojson.feature(topology, topology.objects.subunits))
+//    .attr('d', d3.geo.path().projection(d3.geo.mercator()));
+
+
+// =================================================== start server
 
 var PORT = 8080;
 
 var handleRequest = function (request, response) {
-        response.end('<h1>Animals</h1>' + animalTypes);
-    },
-    server = http.createServer(handleRequest);
+        response.end(result);
+    };
+
+var server = http.createServer(handleRequest);
 
 server.listen(PORT, function () {
     console.log('Server listening on: http://localhost:%s', PORT);
